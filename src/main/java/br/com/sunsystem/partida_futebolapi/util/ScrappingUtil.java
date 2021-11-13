4655278deab27d02b2f0094c7a0ae6224d27055a
adesdsilva <adesdsilva@gmail.com>
2021-11-12 23:37:12 -0300

    Conectando Json com o google

--- src/main/java/br/com/sunsystem/partida_futebolapi/util/ScrappingUtil.java
+++ src/main/java/br/com/sunsystem/partida_futebolapi/util/ScrappingUtil.java
@@ -1,13 +1,40 @@
 package br.com.sunsystem.partida_futebolapi.util;
 
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import br.com.sunsystem.partida_futebolapi.dto.PartidaGoogleDTO;
+
 public class ScrappingUtil {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(ScrappingUtil.class);
 	public static final String BASE_URL_GOOGLE = "http://www.google.com/search?q=";
 	private static final String COMPLEMENTO_URL_GOOGLE = "&hl=pt-BR";
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String url = BASE_URL_GOOGLE + "flamengo+x+palmeiras+27/11/2021" + COMPLEMENTO_URL_GOOGLE;
+		ScrappingUtil scrapping = new ScrappingUtil();
+		scrapping.obterInfoPartida(url);
 
+	}
+	
+	public PartidaGoogleDTO obterInfoPartida(String url) {
+		PartidaGoogleDTO partida = new PartidaGoogleDTO();
+		Document document = null;
+		try {
+			document = Jsoup.connect(url).get();
+			String title = document.title();
+			LOGGER.info("Titulo da pagina: {}", title);
+			
+			
+		} catch (IOException e) {
+			LOGGER.error("Erro ao conectar no Google! -> {}", e.getMessage());
+		}
+		return partida;
 	}
 
 }
