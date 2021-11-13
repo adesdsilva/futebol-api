3c04598ffe0867f0a8e74bef237e2f3355a851d4
adesdsilva <adesdsilva@gmail.com>
2021-11-12 22:20:06 -0300

    subindo versao inicial futebol-api

---
+++ src/main/java/br/com/sunsystem/partida_futebolapi/PartidaFutebolApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.sunsystem.partida_futebolapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartidaFutebolApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartidaFutebolApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/sunsystem/partida_futebolapi/PartidaFutebolApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sunsystem.partida_futebolapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartidaFutebolApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
